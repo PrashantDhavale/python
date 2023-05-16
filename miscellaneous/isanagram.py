c2fa2cf30cde45027c00d27b23f64987a50110f3
Prashant Dhavale <pndhavale@gmail.com>
2023-05-16 21:23:01 +1000

    Practice

--- miscellaneous/isanagram.py
+++ miscellaneous/isanagram.py
@@ -1,34 +1,55 @@
-'''Scenario
-An anagram is a new word formed by rearranging the letters of a word, using all the original letters exactly once. For example, the phrases "rail safety" and "fairy tales" are anagrams, while "I am" and "You are" are not.
+# Scenario
+# An anagram is a new word formed by rearranging the letters of a word, using all the original letters exactly once. For example, the phrases "rail safety" and "fairy tales" are anagrams, while "I am" and "You are" are not.
+#
+# Your task is to write a program which:
+#
+# asks the user for two separate texts;
+# checks whether, the entered texts are anagrams and prints the result.
+# Note:
+#
+# assume that two empty strings are not anagrams;
+# treat upper- and lower-case letters as equal;
+# spaces are not taken into account during the check - treat them as non-existent
+# Test your code using the data we've provided.
+#
+# Test data
+# Sample input:
+#
+# Listen
+# Silent
+#
+# Sample output:
+#
+# Anagrams
+#
+#
+# Sample input:
+#
+# modern
+# norman
+#
+# Sample output:
+#
+# Not anagrams
 
-Your task is to write a program which:
+firstWord = input("Enter first word: ").upper()
+secondWord = input("Enter second word: ").upper()
 
-asks the user for two separate texts;
-checks whether, the entered texts are anagrams and prints the result.
-Note:
+if firstWord.strip() == '' or secondWord.strip() == '':
+    print("Not anagrams")
+foundAll = False
+for char in firstWord:
+    if char.isspace():
+        continue
+    if char.isalpha():
+        if secondWord.find(char) != -1:
+            foundAll = True
+            continue
+        else:
+            foundAll = False
+            break
 
-assume that two empty strings are not anagrams;
-treat upper- and lower-case letters as equal;
-spaces are not taken into account during the check - treat them as non-existent
-Test your code using the data we've provided.
-
-Test data
-Sample input:
-
-Listen
-Silent
-
-Sample output:
-
-Anagrams
-
-
-Sample input:
-
-modern
-norman
-
-Sample output:
-
-Not anagrams'''
-
+if foundAll:
+    print("Anagrams")
+else:
+    print("Not anagrams")
