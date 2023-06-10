6617445bd5204f595a1eb3fd0a567c6ca00fc4b1
Prashant Dhavale <pndhavale@gmail.com>
2023-06-10 12:37:24 +1000

    Create point.py

---
+++ miscellaneous/point.py
@@ -0,0 +1,28 @@
+import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x__ = x
+        self.__y__ = y
+
+    def getx(self):
+        return self.__x__
+
+    def gety(self):
+        return self.__y__
+
+    def distance_from_xy(self, x, y):
+        c = math.sqrt(math.pow(self.__x__, 2) + math.pow(self.__y__, 2))
+        return c
+
+    @staticmethod
+    def distance_from_point(point):
+        c = point.distance_from_xy(point.getx(), point.gety())
+        return c
+
+
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
