62f108779bca3750879d58e7937056b07c294757
Prashant Dhavale <pndhavale@gmail.com>
2024-02-06 14:38:11 +1100

    Update combinations

--- agl/combinations2.py
+++ agl/combinations2.py
@@ -2,49 +2,41 @@ import json
 import itertools
 
 params_info = {
-    "x": [1, 2, 3],
-    "y": ["a", "b", "c"],
-    "z": ["A", "B", "C"],
+
     "dvValues": [True, False],
     "regValues": [True, False],
     "hasDobValues": [True, False],
     "hasAgeValues": [True, False],
-    "deceasedValues": [True, False],
-    "relTypeValues": ["OWNER", "SECONDARY_USER", "NOMINATED_REPRESENTATIVE"],
+    "deceasedValues": [True, False]
 }
 
 for param_vals in itertools.product(*params_info.values()):
     params = dict(zip(params_info.keys(), param_vals))
 
     data = {
-        "relationships": [
-            {
-                "type": params["relTypeValues"]
-            }
-        ],
         "eligibility": [
             {
-                "type": "dvStatus",
+                "name": "dvStatus",
                 "value": params["dvValues"]
             },
             {
-                "type": "registrationStatus",
+                "name": "registrationStatus",
                 "value": params["regValues"]
             },
             {
-                "type": "hasDob",
+                "name": "hasDob",
                 "value": params["hasDobValues"]
             },
             {
-                "type": "hasAge",
+                "name": "hasAge",
                 "value": params["hasAgeValues"]
             },
             {
-                "type": "deceased",
+                "name": "deceased",
                 "value": params["deceasedValues"]
-            },
+            }
         ]
     }
 
-    jsonStr = json.dumps(data)
+    jsonStr = json.dumps(data) + ","
     print(jsonStr)
